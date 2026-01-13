027c9fd8c9dc82e6be69cb56805eedc7af5cb877
gg <chown_tee@proton.me>
2026-01-12 21:21:21 -0500

    fix error on Ubuntu 24.04 and Qt6.4:
    [ 54%] Built target feather_autogen
    [ 55%] Building CXX object src/CMakeFiles/feather.dir/feather_autogen/mocs_compilation.cpp.o
    [ 55%] Building CXX object src/CMakeFiles/feather.dir/ContactsWidget.cpp.o
    [ 55%] Building CXX object src/CMakeFiles/feather.dir/MainWindow.cpp.o
    [ 55%] Building CXX object src/CMakeFiles/feather.dir/HistoryWidget.cpp.o
    [ 55%] Building CXX object src/CMakeFiles/feather.dir/ReceiveWidget.cpp.o
    [ 55%] Building CXX object src/CMakeFiles/feather.dir/WindowManager.cpp.o
    ^[[1;5A^[[1;5A[ 55%] Building CXX object src/CMakeFiles/feather.dir/libwalletqt/Wallet.cpp.o
    [ 55%] Building CXX object src/CMakeFiles/feather.dir/model/AddressBookProxyModel.cpp.o
    [ 55%] Building CXX object src/CMakeFiles/feather.dir/model/HistoryView.cpp.o
    [ 55%] Building CXX object src/CMakeFiles/feather.dir/model/SubaddressProxyModel.cpp.o
    [ 55%] Building CXX object src/CMakeFiles/feather.dir/model/TransactionHistoryProxyModel.cpp.o
    [ 56%] Building CXX object src/CMakeFiles/feather.dir/utils/WebsocketClient.cpp.o
    [ 56%] Building CXX object src/CMakeFiles/feather.dir/utils/daemonrpc.cpp.o
    [ 56%] Building CXX object src/CMakeFiles/feather.dir/utils/nodes.cpp.o
    [ 57%] Building CXX object src/CMakeFiles/feather.dir/utils/os/tails.cpp.o
    [ 57%] Building CXX object src/CMakeFiles/feather.dir/utils/os/whonix.cpp.o
    [ 57%] Building CXX object src/CMakeFiles/feather.dir/utils/prices.cpp.o
    [ 57%] Building CXX object src/CMakeFiles/feather.dir/utils/scheduler.cpp.o
    [ 57%] Building CXX object src/CMakeFiles/feather.dir/utils/textedit.cpp.o
    [ 59%] Building CXX object src/CMakeFiles/feather.dir/widgets/NetworkProxyWidget.cpp.o
    [ 59%] Building CXX object src/CMakeFiles/feather.dir/widgets/NodeWidget.cpp.o
    [ 59%] Building CXX object src/CMakeFiles/feather.dir/widgets/PasswordSetWidget.cpp.o
    [ 59%] Building CXX object src/CMakeFiles/feather.dir/widgets/PayToEdit.cpp.o
    [ 60%] Building CXX object src/CMakeFiles/feather.dir/widgets/PluginWidget.cpp.o
    /home/mark/Desktop/feather/src/widgets/NodeWidget.cpp: In constructor ‘NodeWidget::NodeWidget(QWidget*)’:
    /home/mark/Desktop/feather/src/widgets/NodeWidget.cpp:24:53: error: ‘checkStateChanged’ is not a member of ‘QCheckBox’
       24 |     connect(ui->checkBox_websocketList, &QCheckBox::checkStateChanged, [this](Qt::CheckState state){
          |                                                     ^~~~~~~~~~~~~~~~~
    make[2]: *** [src/CMakeFiles/feather.dir/build.make:2183: src/CMakeFiles/feather.dir/widgets/NodeWidget.cpp.o] Error 1
    make[2]: *** Waiting for unfinished jobs....
    make[1]: *** [CMakeFiles/Makefile2:2248: src/CMakeFiles/feather.dir/all] Error 2
    make: *** [Makefile:136: all] Error 2

--- src/widgets/NodeWidget.cpp
+++ src/widgets/NodeWidget.cpp
@@ -21,7 +21,7 @@ NodeWidget::NodeWidget(QWidget *parent)
 
     connect(ui->btn_addCustomNodes, &QPushButton::clicked, this, &NodeWidget::onCustomAddClicked);
 
-    connect(ui->checkBox_websocketList, &QCheckBox::checkStateChanged, [this](Qt::CheckState state){
+    connect(ui->checkBox_websocketList, &QCheckBox::stateChanged, [this](int state){
         bool custom = (state == Qt::Unchecked);
         ui->stackedWidget->setCurrentIndex(custom);
         ui->frame_addCustomNodes->setVisible(custom);
