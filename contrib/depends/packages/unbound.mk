8df4900c1774756af367ec170c20bd65b8c20965
tobtoht <tob@featherwallet.org>
2026-02-04 19:47:17 +0100

    depends: unbound: update to 1.24.2

--- contrib/depends/packages/unbound.mk
+++ contrib/depends/packages/unbound.mk
@@ -1,8 +1,8 @@
 package=unbound
-$(package)_version=1.24.1
+$(package)_version=1.24.2
 $(package)_download_path=https://www.nlnetlabs.nl/downloads/$(package)/
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
-$(package)_sha256_hash=7f2b1633e239409619ae0527f67878b0f33ae0ec0ee5a3a51c042c359ba1eeab
+$(package)_sha256_hash=44e7b53e008a6dcaec03032769a212b46ab5c23c105284aa05a4f3af78e59cdb
 $(package)_dependencies=openssl expat
 
 define $(package)_set_vars
